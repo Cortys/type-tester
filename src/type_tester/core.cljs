168735738bc4d6278144114b488703a72e4d2b69
Clemens Damke <admin@cortys.de>
2017-05-17 22:30:52 +0200

    added number mode

--- src/type_tester/core.cljs
+++ src/type_tester/core.cljs
@@ -3,29 +3,53 @@
             [amalloy.ring-buffer :refer [ring-buffer]]))
 
 (def buf-size 5)
-
-(def state (r/atom {:count 0
+(def state (r/atom {:use-numbers false
+                    :count 0
                     :tasks (ring-buffer buf-size)}))
+
+(declare fill-tasks!)
 
 (defn type-task
   [task]
   [:div {:class "type-task", :key (:id task)}
-   (:letter task)])
+        (:letter task)])
+
+(defn controls
+  []
+  [:div {:id "controls"}
+        [:input {:id "useNumbers"
+                 :type "checkbox"
+                 :checked (:use-numbers @state)
+                 :on-change #(do (swap! state
+                                        assoc :use-numbers (-> % .-target .-checked))
+                                 (fill-tasks!))}]])
 
 (defn content
   []
   (let [state @state]
-       [:div {:id "letters"}
-        (map type-task (:tasks state))]))
+       [:div {:id "content"}
+             [controls]
+             [:div {:id "letters"}
+                   (map type-task (:tasks state))]]))
 
 (defn current-task
   []
   (second (:tasks @state)))
 
+(defn generate-letter
+  []
+  (char (if (= (:use-numbers @state) true)
+          (+ 48 (rand-int 10))
+          (+ 65 (rand-int 26)))))
+
 (defn next-task!
   []
   (swap! state update :tasks conj {:id (:count (swap! state update :count inc))
-                                   :letter (char (+ 65 (rand-int 26)))}))
+                                   :letter (generate-letter)}))
+
+(defn fill-tasks!
+  []
+  (doall (repeatedly buf-size next-task!)))
 
 (defn key-pressed-handler!
   [event]
@@ -34,9 +58,9 @@
 
 (defn init!
   []
-  (r/render [content] (.getElementById js/document "content"))
+  (r/render [content] (.getElementById js/document "host"))
   (.addEventListener js/document "keydown" key-pressed-handler!)
 
-  (doall (repeatedly buf-size next-task!)))
+  (fill-tasks!))
 
 (init!)
